379f3458b75660dcdbbf8de145e8d41e964393a5
S567071 <S567071@nwmissouri.edu>
2023-11-03 03:06:00 -0500

    mongodb

---
+++ README.md
@@ -0,0 +1,5 @@
+# f23wb71jeedipally
+web apps lab 10
+Render link : https://f23wb71jeedipally.onrender.com
+# class - Apartment
+# attributes - Apartment_name(string), location(string), Rent(number)
---
+++ routes/board.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('board', { title: 'Board Display' });
+});
+
+/* Handle query parameters for the '/gridbuild' route */
+router.get('/gridbuild', function(req, res, next) {
+  let query = req.query;
+  console.log(`rows ${query.rows}`);
+  console.log(`cols ${query.cols}`);
+  res.render('board', { title: 'Board Display', query: query });
+});
+
+module.exports = router;
