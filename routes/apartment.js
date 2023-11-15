abb19ece57dbd499c375d8ddb7930bf01cd529ab
S567071 <S567071@nwmissouri.edu>
2023-11-15 14:40:22 -0600

    create

--- routes/apartment.js
+++ routes/apartment.js
@@ -12,4 +12,7 @@ router.get('/', apartment_controlers.apartment_view_all_Page );
 /* GET detail costume page */
 router.get('/detail', apartment_controlers.apartment_view_one_Page);
 
+/* GET create costume page */
+router.get('/create', apartment_controlers.apartment_create_Page);
+
 module.exports = router;
