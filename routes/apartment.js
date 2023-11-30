7335609c80eda945c0dbb58087b9a5d8acff2553
S567071 <S567071@nwmissouri.edu>
2023-11-29 22:20:54 -0600

    button

--- routes/apartment.js
+++ routes/apartment.js
@@ -6,16 +6,6 @@ var router = express.Router();
 /*router.get('/', function(req, res, next) {
   res.render('apartment', { title: 'Search Results apartment' });
 });*/
-
-router.get('/', apartment_controlers.apartment_view_all_Page );
-
-/* GET detail costume page */
-router.get('/detail', apartment_controlers.apartment_view_one_Page);
-
-/* GET create costume page */
-router.get('/create', apartment_controlers.apartment_create_Page);
-
-/* GET create update page */
 const secured = (req, res, next) => {
   if (req.user) {
       return next();
@@ -23,11 +13,22 @@ const secured = (req, res, next) => {
   res.redirect("/login");
 }
 
+router.get('/', apartment_controlers.apartment_view_all_Page );
+
+/* GET detail costume page */
+router.get('/detail',secured, apartment_controlers.apartment_view_one_Page);
+
+/* GET create costume page */
+router.get('/create',secured, apartment_controlers.apartment_create_Page);
+
+/* GET create update page */
+
+
 router.get('/update', secured, apartment_controlers.apartment_update_Page);
 
 
 /* GET delete apartment page */
-router.get('/delete', apartment_controlers.apartment_delete_Page);
+router.get('/delete', secured, apartment_controlers.apartment_delete_Page);
 
 module.exports = router;
 
